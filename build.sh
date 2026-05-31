28b2c81b68b6e1b8748790c0360d78004f001fc6
projetowebsiteux20261-del <projetowebsiteux20261@gmail.com>
2026-05-31 20:47:44 +0000

    feat: gera arquivos de config via build.sh na Vercel

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+cat > js/js/firebase-config-values.js << CONF
+export const firebaseConfig = {
+  apiKey:            "${FIREBASE_API_KEY}",
+  authDomain:        "${FIREBASE_AUTH_DOMAIN}",
+  projectId:         "${FIREBASE_PROJECT_ID}",
+  storageBucket:     "${FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${FIREBASE_MESSAGING_ID}",
+  appId:             "${FIREBASE_APP_ID}"
+};
+CONF
+
 cat > js/config.js << CONF
 export const GOOGLE_BOOKS_KEY = '${GOOGLE_BOOKS_KEY}';
 export const TMDB_API_KEY     = '${TMDB_API_KEY}';
