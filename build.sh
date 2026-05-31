3600f68e4498edbe12158bf2e52a77db9f9abaaa
projetowebsiteux20261-del <projetowebsiteux20261@gmail.com>
2026-05-31 22:42:03 +0000

    fix: corrige nome da variavel FIREBASE_MESSAGING_SENDER_ID

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export const firebaseConfig = {
   authDomain:        "${FIREBASE_AUTH_DOMAIN}",
   projectId:         "${FIREBASE_PROJECT_ID}",
   storageBucket:     "${FIREBASE_STORAGE_BUCKET}",
-  messagingSenderId: "${FIREBASE_MESSAGING_ID}",
+  messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
   appId:             "${FIREBASE_APP_ID}"
 };
 CONF
