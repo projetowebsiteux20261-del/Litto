694dfd90d1d87c1236c00c72500a5c40058dbb69
projetowebsiteux20261-del <projetowebsiteux20261@gmail.com>
2026-06-01 17:18:01 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const GOOGLE_BOOKS_KEY = 'AIzaSyCVQpyUbZrBl1yiIsRfoDCJNx0FLRUQ2fU';
-export const TMDB_API_KEY     = '7dbf7128baf22521877136df552400b5';
+export const GOOGLE_BOOKS_KEY = '';
+export const TMDB_API_KEY     = '';
